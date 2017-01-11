aa2e6013093e4d832aaac819477ef47a7fcaf0d2
jinpeng <jinpeng@sodao.com>
2017-01-11 14:08:09 +0800

    md5加密，redishelper

---
+++ Jin.Tools/Cache/Redis/RedisHelper.cs
@@ -0,0 +1,25 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class RedisHelper
+    {
+        private static string host = GetConfigMsgHelper.GetValueByKey("host");
+        private static string port = GetConfigMsgHelper.GetValueByKey("port");
+        private static string pwd = GetConfigMsgHelper.GetValueByKey("pwd");
+        private static string db = GetConfigMsgHelper.GetValueByKey("db");
+
+        public static RedisClient redisClient;
+
+        static RedisHelper()
+        {
+            redisClient = new RedisClient(host, Convert.ToInt32(port), pwd, Convert.ToInt64(db));
+        }
+    }
+
+}
---
+++ Jin.Tools/Md5/Md5Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public  static class Md5Helper
+    {
+        /// <summary>
+        /// 给字符串进行MD5加密运算，并放回加密后的字符串
+        /// </summary>
+        /// <param name="sDataIn"></param>
+        /// <returns></returns>
+        public static string GetMD5(string sDataIn)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(sDataIn);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+        /// <summary>
+        /// 比较Md5字符串是否相等
+        /// </summary>
+        /// <param name="inputPwd">用户输入的密码字符串</param>
+        /// <param name="dataBasePwd">数据库中的加密字符串</param>
+        /// <returns></returns>
+        public static bool VerifyMd5Hash(string inputPwd, string dataBasePwd)
+        {
+            if (GetMD5(inputPwd) == dataBasePwd)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
